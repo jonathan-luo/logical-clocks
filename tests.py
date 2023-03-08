54b4329988325f008c62a25bb357820bdce85efd
Michael Hu <43020369+GuessHuIAm@users.noreply.github.com>
2023-03-08 12:42:31 -0500

    added more unit tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import csv
 from multiprocessing import Process
 from unittest.mock import patch
 from utils import machine, server
@@ -9,14 +10,12 @@ from constants import *
 
 class TestProgram(unittest.TestCase):
     def setUp(self):
-        self.p1_log_path = "p1_log.csv"
-        self.p2_log_path = "p2_log.csv"
-        self.p3_log_path = "p3_log.csv"
+        self.log_paths = ["p1_log.csv", "p2_log.csv", "p3_log.csv"]
 
         config1 = [
         (LOCALHOST, PORT_1),
         [(LOCALHOST, PORT_2), (LOCALHOST, PORT_3)],
-        self.p1_log_path
+        self.log_paths[0]
         ]
         self.p1_process = Process(target=machine, args=(config1,))
         
@@ -24,14 +23,14 @@ class TestProgram(unittest.TestCase):
         config2 = [
             (LOCALHOST, PORT_2),
             [(LOCALHOST, PORT_1), (LOCALHOST, PORT_3)],
-            self.p2_log_path
+            self.log_paths[1]
         ]
         self.p2_process = Process(target=machine, args=(config2,))
         
         config3 = [
             (LOCALHOST, PORT_3),
             [(LOCALHOST, PORT_1), (LOCALHOST, PORT_2)],
-            self.p3_log_path
+            self.log_paths[2]
         ]
         self.p3_process = Process(target=machine, args=(config3,))
         
@@ -42,33 +41,61 @@ class TestProgram(unittest.TestCase):
         # Wait for everything to initialize
         sleep(INITIAL_WAIT_TIME)
         
-    
-    def tearDown(self):
+        # Stop the processes after we have sizeable log files
+        sleep(10)
         self.p1_process.terminate()
         self.p2_process.terminate()
         self.p3_process.terminate()
-
-        os.remove(self.p1_log_path)
-        os.remove(self.p2_log_path)
-        os.remove(self.p3_log_path)
+        
+    
+    def tearDown(self):
+        for log_path in self.log_paths:
+            if os.path.exists(log_path):
+                os.remove(log_path)
         
         
-    def test_run_model(self):   
+    def test_log_files_exists(self):   
         # Check if log files are created and not empty
-        sleep(1)
-        self.assertTrue(os.path.exists(self.p1_log_path))
-        self.assertGreater(os.path.getsize(self.p1_log_path), 0)
-        self.assertTrue(os.path.exists(self.p2_log_path))
-        self.assertGreater(os.path.getsize(self.p2_log_path), 0)
-        self.assertTrue(os.path.exists(self.p3_log_path))
-        self.assertGreater(os.path.getsize(self.p3_log_path), 0)
-        
-        print("Passed test_run_model()")
-        
+        for log_path in self.log_paths:
+            self.assertTrue(os.path.exists(log_path))
+            self.assertGreater(os.path.getsize(log_path), 0)
+
+
+    def test_log_file_format(self):
+        for log_path in self.log_paths:
+            with open(log_path, 'r') as file:
+                reader = csv.reader(file)
+                
+                # Check header row
+                header_row = next(reader)
+                expected_header = ["Operation", "Global Time", "Logical Time", "Length of Message Queue"]
+                self.assertEqual(header_row, expected_header)
+                
+                # Check number of columns in each row
+                for row in reader:
+                    expected_num_columns = len(expected_header)
+                    self.assertEqual(len(row), expected_num_columns)
+
+      
+    def test_log_file_content(self):
+        # Check if log file contains the correct operations
+        for log_path in self.log_paths:
+            with open(log_path, 'r') as file:
+                reader = csv.reader(file)
+                
+                # Skip header row
+                next(reader)
+                
+                # Check if each row contains the correct operation
+                for row in reader:
+                    operation, global_time, logical_time, queue_len = row
+                    self.assertIn(operation, ["Send (1)", "Send (2)", "Send (3)", "Internal Event", "Receive"])
+                    
+                    if operation == "Receive":
+                        self.assertNotEqual(global_time, "N/A")
+                    else:
+                        self.assertEqual(queue_len, "N/A")
 
 if __name__ == '__main__':
-    program = TestProgram()
-    program.setUp()
-    program.test_run_model()
-    program.tearDown()
+    unittest.main()
     
