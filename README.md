4d723dac42f2b258797e9a9a44459e1442250416
Michael Hu <43020369+GuessHuIAm@users.noreply.github.com>
2023-03-08 02:23:57 -0500

    started some tests and edited notebook

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ To make sure you have all the required modules for this application, run `pip in
 
 ### Running Model
 Run `python model.py`. Logs of the interprocess communication and internal events for virtual machine 1, 2, and 3 will be stored in `p1_log.txt`, `p2_log.txt`, and `p3_log.txt`, respectively.
+
+### Running Tests
+Run `python tests.py` to run the unit tests for this program.
---
+++ tests.py
@@ -0,0 +1,64 @@
+import os
+import unittest
+import socket
+from multiprocessing import Process
+from unittest.mock import patch
+from utils import machine, server
+from time import sleep
+from constants import *
+
+
+class TestProgram(unittest.TestCase):
+    def setUp(self):
+        self.p1_log_path = "p1_log.csv"
+        self.p2_log_path = "p2_log.csv"
+        self.p3_log_path = "p3_log.csv"
+
+        config1 = [
+        (LOCALHOST, PORT_1),
+        [(LOCALHOST, PORT_2), (LOCALHOST, PORT_3)],
+        "p1_log.csv"
+        ]
+        
+        self.p1_process = Process(target=machine, args=(config1,)).start()
+        
+        config2 = [
+            (LOCALHOST, PORT_2),
+            [(LOCALHOST, PORT_1), (LOCALHOST, PORT_3)],
+            "p2_log.csv"
+        ]
+        self.p2_proces = Process(target=machine, args=(config2,)).start()
+        
+        config3 = [
+            (LOCALHOST, PORT_3),
+            [(LOCALHOST, PORT_1), (LOCALHOST, PORT_2)],
+            "p3_log.csv"
+        ]
+        self.p3_proces = Process(target=machine, args=(config3,)).start()
+
+    def tearDown(self):
+        self.p1_process.terminate()
+        self.p2_process.terminate()
+        self.p3_process.terminate()
+
+        os.remove(self.p1_log_path)
+        os.remove(self.p2_log_path)
+        os.remove(self.p3_log_path)
+
+    def test_run_model(self):
+        # Check if log files are created and not empty
+        sleep(5)
+        self.assertTrue(os.path.exists(self.p1_log_path))
+        self.assertGreater(os.path.getsize(self.p1_log_path), 0)
+        self.assertTrue(os.path.exists(self.p2_log_path))
+        self.assertGreater(os.path.getsize(self.p2_log_path), 0)
+        self.assertTrue(os.path.exists(self.p3_log_path))
+        self.assertGreater(os.path.getsize(self.p3_log_path), 0)
+        
+
+if __name__ == '__main__':
+    program = TestProgram()
+    program.setUp()
+    program.test_run_model()
+    program.tearDown()
+    
